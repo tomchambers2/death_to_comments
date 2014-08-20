b6889da1ad1c47f45875b3959d3acf1fd895b81b
Tom Chambers <tom.chambers@gmail.com>
2014-08-20 13:05:48 +0100

    Basic functionality working, kills comments but nothing else

---
+++ contentscript.js
@@ -0,0 +1,28 @@
+console.log('we loaded')
+
+var killComments = function() {
+	console.log('hi')
+	var values = ['comments','comments_entry','comment','comment-text','comment-list-module'];
+	var elements = [];
+	for (var i=0;i<values.length;i+=1) {
+		if (document.getElementById(values[i])) {
+			elements.push(document.getElementById(values[i]));
+		};
+		if (document.getElementsByClassName(values[i])) {
+			var classes = document.getElementsByClassName(values[i]);
+			for (var l=0;l<classes.length;l+=1) {
+				elements.push(classes[l]);
+				console.log('added',classes[l]);
+			}
+		}
+	}
+
+	console.log(elements);
+
+	for (var j=0;j<elements.length;j+=1) {
+		console.log('will kill',elements[j])
+		elements[j].style.display = 'none';
+	}
+};
+
+killComments();
